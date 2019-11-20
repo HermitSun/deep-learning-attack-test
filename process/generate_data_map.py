4f0d5f0ff5de842e071740406ab5bfcf8a4fad16
Wen Sun <syl1887415157@126.com>
2019-11-21 00:17:29 +0800

    fix: fix data label map error

--- process/generate_data_map.py
+++ process/generate_data_map.py
@@ -4,13 +4,13 @@ import numpy as np
 from tqdm import tqdm
 
 (train_data, train_labels), (t_data, test_labels) = fashion_mnist.load_data()
-train_data = train_data.astype("float32") / 255
+train_data = train_data.reshape((len(train_data), 28, 28, 1)).astype("float32") / 255
 test_data = np.load("../test_data/test_data.npy")
 
 data_label_map = dict()
 data_index_map = dict()
 for i in tqdm(range(len(train_data))):
-    data_label_map[str(train_data[i])] = train_data[i]
+    data_label_map[str(train_data[i])] = train_labels[i]
     data_index_map[str(train_data[i])] = i
 for i in tqdm(range(len(test_data))):
     data_label_map[str(test_data[i])] = test_labels[i]
@@ -20,8 +20,3 @@ with open("data_label_map.dat", "wb") as l_map:
     pickle.dump(data_label_map, l_map)
 with open("data_index_map.dat", "wb") as i_map:
     pickle.dump(data_index_map, i_map)
-
-with open("data_label_map.dat", "rb") as l_map:
-    data_label_map = pickle.load(l_map)
-with open("data_index_map.dat", "rb") as i_map:
-    data_index_map = pickle.load(i_map)
